929035e83a41491f4aca0f56f6fd7d3cc1dbfbc5
jorhdan <dil@192.168.1.129>
2026-03-15 08:49:45 +0300

    add toggleDrawer on staff headaer component

--- src/components/sidebars/StaffSidebar.jsx
+++ src/components/sidebars/StaffSidebar.jsx
@@ -12,7 +12,7 @@ import {
 } from "@heroicons/react/24/outline";
 import JobzipaLogo from "../../assets/logos/jobzipa.png";
 
-const StaffSidebar = ({ closeDrawer }) => {
+const StaffSidebar = ({ toggleDrawer }) => {
 
   const navigate = useNavigate();
 
