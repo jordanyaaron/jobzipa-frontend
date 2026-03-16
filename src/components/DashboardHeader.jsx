eb56179909a27a5bb10742b618be1c3f52e12b80
jorhdan <dil@DiLs-MacBook-Air.local>
2026-03-16 13:28:26 +0300

    rechange dashboard header

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,50 +1,25 @@
 import JobzipaLogo from "../assets/logos/jobzipa.png";
-import {Bars3Icon} from '@heroicons/react/24/outline'
-export default function DashboardHeader({ setSidebarOpen }) {
-    return (
-      <header className="md:hidden h-16 flex flex-row  items-left px-4 border-b border-[var(--border)] bg-[var(--background)]">
-  
-        {/* Logo */}
-        {/* <span className="font-bold">JOBZPA</span> */}
-        <div 
-            className="
-                flex-1
-                items-center
-                h-16
-            "
-        >
-            <img 
-                src={JobzipaLogo} 
-                className="
-                    h-9
-                "
-                alt="" srcset=""
-             />
-        </div>
-        <div 
-            className="
-                w-[80px]
-                items-center
-                h-16
-            "
-        >
-          <button
-              onClick={setSidebarOpen}
-              className="
-                lg:hidden p-2 rounded-lg cursor-pointer
-                hover:bg-[var(--hover)]  text-[var(--text)] 
-              "
-            >
-              <Bars3Icon className="h-6 w-6  text-[var(--text)]" />
-          </button>
-        </div>
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
-          
-        
-  
-        {/* Spacer */}
-        <div />
-  
-      </header>
-    );
-  }
+export default function DashboardHeader({ setSidebarOpen }) {
+  return (
+    <header className="md:hidden h-16 flex items-center justify-between px-4 border-b border-[var(--border)] bg-[var(--background)]">
+
+      {/* Logo */}
+      <img
+        src={JobzipaLogo}
+        className="h-9"
+        alt="Jobzipa"
+      />
+
+      {/* Menu Button */}
+      <button
+        onClick={setSidebarOpen}
+        className="p-2 rounded-lg cursor-pointer hover:bg-[var(--hover)]"
+      >
+        <Bars3Icon className="h-6 w-6 text-[var(--text)]" />
+      </button>
+
+    </header>
+  );
+}
