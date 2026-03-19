bce46614fcea1f52897aa5dbb07a1ee6fdc90f0e
jorhdan <dil@DiLs-MacBook-Air.local>
2026-03-19 15:29:40 +0300

    remove min-height on job tamble

--- src/pages/dashboard/super/Jobs.jsx
+++ src/pages/dashboard/super/Jobs.jsx
@@ -61,7 +61,7 @@ export default function JobsSuper() {
         </div>
       </div>
       {/* Scrollable row */}
-      <div className="flex mt-16 lg:mt-0  min-h-[calc(100vh-64px)]  overflow-x-auto gap-3 border border-[var(--border)]  rounded-lg">
+      <div className="flex mt-16 lg:mt-0   overflow-x-auto gap-3 border border-[var(--border)]  rounded-lg">
       <table className="min-w-[700px] w-full text-sm">
             <thead className="bg-[var(--hover)] text-left">
               <tr>
