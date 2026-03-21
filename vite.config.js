7d13c7811b053bdbc5f4ef1ca0d8b72c10c430ee
jorhdan <dil@192.168.1.146>
2026-03-21 08:11:28 +0300

    add data folder

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : process.env.VITE_BASE_PATH || '/jobzipa-frontend'
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  base: process.env.VITE_BASE_PATH || '/jobzipa-frontend'
 })
 
 
