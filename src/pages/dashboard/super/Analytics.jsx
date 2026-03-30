a574c0e6101c9f1808969862848319fe5e9241a0
jorhdan <dil@DiLs-MacBook-Air.local>
2026-03-30 13:04:44 +0300

    removing the tab div from analytics page

--- src/pages/dashboard/super/Analytics.jsx
+++ src/pages/dashboard/super/Analytics.jsx
@@ -17,25 +17,6 @@ export default function AnalyticsSuper() {
     <div className="mt-[64px] lg:m-0 p-4 lg:p-6 w-[calc(100vw)] lg:w-[calc(100vw-240px)] space-y-4">
 
       <h1 className="hidden lg:block text-2xl font-bold">Analytics</h1>
-
-      {/* Tabs */}
-      <div className="flex gap-2 overflow-x-auto">
-        {tabs.map((tab) => (
-          <button
-            key={tab.id}
-            onClick={() => setActiveTab(tab.id)}
-            className={`
-              px-4 py-2 rounded-lg text-sm whitespace-nowrap
-              ${activeTab === tab.id 
-                ? "bg-black text-white" 
-                : "bg-[var(--hover)] text-[var(--text)]"}
-            `}
-          >
-            {tab.label}
-          </button>
-        ))}
-      </div>
-
       {/* Content */}
       <div className="border border-[var(--border)] rounded-lg p-4 bg-[var(--background)]">
         {analyticsFilter === "revenue" && <Reveniew />}
