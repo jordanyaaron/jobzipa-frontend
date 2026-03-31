8786dd794e9897cc5a6707b8879fe2d7ab07519c
jorhdan <dil@DiLs-MacBook-Air.local>
2026-03-31 16:56:27 +0300

    Could not resolve "../assets/logos/jobzipa.png" from "src/components/sidebars/StaffDashboardSidebar.jsx"

--- src/components/sidebars/StaffDashboardSidebar.jsx
+++ src/components/sidebars/StaffDashboardSidebar.jsx
@@ -1,5 +1,5 @@
 import {NavLink , useLocation} from 'react-router-dom' 
-import JobzipaLogo from "../assets/logos/jobzipa.png";
+import JobzipaLogo from "@/assets/logos/jobzipa.png"
 import {
     HomeIcon,
     BriefcaseIcon,
