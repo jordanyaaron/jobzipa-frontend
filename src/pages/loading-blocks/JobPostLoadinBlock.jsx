e75e4a61be95678d9b52186829ca863f66b10c20
jorhdan <dil@192.168.1.7>
2026-04-11 23:25:34 +0300

    add

--- src/pages/loading-blocks/JobPostLoadinBlock.jsx
+++ src/pages/loading-blocks/JobPostLoadinBlock.jsx
@@ -32,15 +32,15 @@
             >
               {/* header */}
               <div className="flex gap-2">
-                <div className="w-10 h-10 border " > <SkeletonBlock className="h-10 w-10 rounded-full" /></div>
+                <div className="w-10 h-10" > <SkeletonBlock className="h-10 w-10 rounded-full" /></div>
                 <div className="flex-1 flex justify-between items-center " > 
-                  <SkeletonBlock className="h-4 w-1/3  rounded-xl" />
-                  <SkeletonBlock className="h-4 w-4 rounded-full" />
+                  <SkeletonBlock className="h-4 w-1/2  rounded-xl" />
+                  <SkeletonBlock className="h-4 w-10 rounded-full" />
                 </div>
               </div>
               <div className="flex flex-col gap-2 my-3">
-                <SkeletonBlock className="h-5 w-full rounded-xl" />
-                <SkeletonBlock className="h-5 w-1/3  rounded-xl" />
+                <SkeletonBlock className="h-5 w-full rounded-sm" />
+                <SkeletonBlock className="h-5 w-1/3  rounded-sm" />
               </div>
               <div className="flex  gap-2 my-3">
                 <SkeletonBlock className="h-6 w-6 rounded-sm" />
