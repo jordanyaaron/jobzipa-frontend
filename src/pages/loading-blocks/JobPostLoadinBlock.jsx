5007ac5689d019df592ee2a88fe816c8f2f3089c
jorhdan <dil@192.168.1.7>
2026-04-11 23:18:36 +0300

    edit

--- src/pages/loading-blocks/JobPostLoadinBlock.jsx
+++ src/pages/loading-blocks/JobPostLoadinBlock.jsx
@@ -14,53 +14,61 @@
   
   export default function JobsSkeleton(){
     return (
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 pt-6">
-        {[...Array(6)].map((_, i) => (
-          <div
-            key={i}
-            className="
-              p-4 
-              space-y-4
-            "
-          >
-            {/* header */}
-            <div className="flex gap-2">
-              <div className="w-[80px] h-[80px] border border-[var(--border)]" > <SkeletonBlock className="h-10 w-10 rounded-full" /></div>
-              <div className="flex-1 flex justify-between items-center " > 
-                <SkeletonBlock className="h-4 w-1/3  rounded-xl" />
-                <SkeletonBlock className="h-4 w-4 rounded-full" />
+      <div
+        className="
+          w-full
+          lg:w-[700px]
+        "
+      >
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 pt-6">
+          {[...Array(6)].map((_, i) => (
+            <div
+              key={i}
+              className="
+                p-4 
+                space-y-4
+                border-b border-[var(--border)]
+              "
+            >
+              {/* header */}
+              <div className="flex gap-2">
+                <div className="w-10 h-10 border " > <SkeletonBlock className="h-10 w-10 rounded-full" /></div>
+                <div className="flex-1 flex justify-between items-center " > 
+                  <SkeletonBlock className="h-4 w-1/3  rounded-xl" />
+                  <SkeletonBlock className="h-4 w-4 rounded-full" />
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col gap-2 my-3">
-              <SkeletonBlock className="h-5 w-full rounded-xl" />
-              <SkeletonBlock className="h-5 w-1/3  rounded-xl" />
-            </div>
-            <div className="flex flex-col gap-2 my-3">
-              <SkeletonBlock className="h-5 w-5 rounded-xl" />
-              <SkeletonBlock className="h-5 w-5  rounded-xl" />
-            </div>
+              <div className="flex flex-col gap-2 my-3">
+                <SkeletonBlock className="h-5 w-full rounded-xl" />
+                <SkeletonBlock className="h-5 w-1/3  rounded-xl" />
+              </div>
+              <div className="flex  gap-2 my-3">
+                <SkeletonBlock className="h-6 w-6 rounded-sm" />
+                <SkeletonBlock className="h-6 w-6  rounded-sm" />
+              </div>
 
-            {/* <SkeletonBlock className="h-5 w-3/4" />
-  
-            <SkeletonBlock className="h-4 w-1/2" />
-  
-            <div className="space-y-2">
-              <SkeletonBlock className="h-3 w-full" />
-              <SkeletonBlock className="h-3 w-5/6" />
-              <SkeletonBlock className="h-3 w-2/3" />
+              {/* <SkeletonBlock className="h-5 w-3/4" />
+    
+              <SkeletonBlock className="h-4 w-1/2" />
+    
+              <div className="space-y-2">
+                <SkeletonBlock className="h-3 w-full" />
+                <SkeletonBlock className="h-3 w-5/6" />
+                <SkeletonBlock className="h-3 w-2/3" />
+              </div>
+    
+              <div className="flex gap-2">
+                <SkeletonBlock className="h-5 w-16 rounded-full" />
+                <SkeletonBlock className="h-5 w-20 rounded-full" />
+              </div>
+    
+              <div className="flex justify-between items-center pt-2">
+                <SkeletonBlock className="h-4 w-20" />
+                <SkeletonBlock className="h-9 w-24 rounded-lg" />
+              </div> */}
             </div>
-  
-            <div className="flex gap-2">
-              <SkeletonBlock className="h-5 w-16 rounded-full" />
-              <SkeletonBlock className="h-5 w-20 rounded-full" />
-            </div>
-  
-            <div className="flex justify-between items-center pt-2">
-              <SkeletonBlock className="h-4 w-20" />
-              <SkeletonBlock className="h-9 w-24 rounded-lg" />
-            </div> */}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     );
   };
