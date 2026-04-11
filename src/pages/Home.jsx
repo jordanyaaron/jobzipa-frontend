725b29a37ce55bffa67b09d3a172731d19d31352
jorhdan <dil@192.168.1.7>
2026-04-11 22:01:06 +0300

    fix error during build: [vite:load-fallback] Could not load /vercel/path0/src/pages/loading-blocks/ (imported by src/pages/Home.jsx): EISDIR: illegal operation on a directory, read

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useuseEffect , useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Nav';
 import Footer from '../components/Foote';
-import JobsSkeleton from '@/pages/loading-blocks/'
+import JobsSkeleton from '@/pages/loading-blocks/JobPostLoadinBlock'
 
 const Home = () => {
   const [ blockSkeleton , setBlockSkeleton ] = useState(true);
