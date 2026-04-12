bd6f4519a587db3826315cd497b6d5d43dec260b
jorhdan <dil@192.168.1.7>
2026-04-12 07:40:52 +0300

    fix error

--- src/pages/loading-blocks/JobPostLoadinBlock.jsx
+++ src/pages/loading-blocks/JobPostLoadinBlock.jsx
@@ -1,15 +1,17 @@
-export default function  SkeletonBlock ({ className = "" }) {
-    <div
-      className={`
-        ${className}
-        rounded
-        bg-gradient-to-r
-        from-[var(--hover)]
-        via-gray-300/40
-        to-[var(--hover)]
-        animate-[pulse_1.5s_ease-in-out_infinite]
-      `}
-    />
+  export default function  SkeletonBlock ({ className = "" }) {
+    return (
+        <div
+        className={`
+          ${className}
+          rounded
+          bg-gradient-to-r
+          from-[var(--hover)]
+          via-gray-300/40
+          to-[var(--hover)]
+          animate-[pulse_1.5s_ease-in-out_infinite]
+        `}
+      />
+    )
   };
   
   export default function JobsSkeleton(){
