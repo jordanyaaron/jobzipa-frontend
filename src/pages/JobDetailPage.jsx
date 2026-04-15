61b9a56bd4f6a0fea05bfeb0295443ff3efdaa86
jorhdan <dil@192.168.1.7>
2026-04-15 08:03:15 +0300

    modify the the past detail view

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -1,25 +1,42 @@
+import SkeletonBlock from "@/components/skeletons/JobZipaSkeleton";
+import  JobsSkeleton  from '@/pages/loading-blocks/JobPostLoadinBlock'
+
 export default function JobDetailPage() {
+
+    const [ blockSkeleton , setBlockSkeleton ] = useState(true);
     return (
-      <div className="space-y-4">
-  
-        {/* Job Header */}
-        <div className="bg-white dark:bg-gray-900 p-5 rounded-xl border">
-          <h1 className="text-2xl font-bold">Job Title</h1>
-          <p className="text-gray-500">Company</p>
-        </div>
-  
-        {/* Description */}
-        <div className="bg-white dark:bg-gray-900 p-5 rounded-xl border">
-          <h2 className="font-semibold mb-2">Description</h2>
-  
-          <div
-            className="text-sm text-gray-700 dark:text-gray-300"
-            dangerouslySetInnerHTML={{
-              __html: "job.description here"
-            }}
-          />
-        </div>
-  
+        <div className="w-fulll flex gap-2" >
+        {
+          blockSkeleton 
+          ?  <JobsSkeleton />
+          :  <main
+                className="
+                  w-full
+                  pt-6
+                  lg:w-[710px] 
+                "
+              >
+                
+              
+              </main>
+        }
+        <aside className="p-6 hidden lg:flex gap-3 flex-col ">
+            <div className=' mt-4 flex-col  flex gap-2'>
+              <SkeletonBlock className="w-20 h-6 "/>
+              <SkeletonBlock className="w-full h-[150px]"/>
+            </div>
+            <div className='flex gap-2'>
+              <div className=''>
+                <SkeletonBlock className="w-[100px] h-[100px]"/>
+              </div>
+              <div className='flex-1 flex flex-col gap-2'>
+                <SkeletonBlock className="h-5 w-full"/>
+                <SkeletonBlock className="h-5 w-3/4"/>
+                <SkeletonBlock className="h-5 w-1/4"/>
+              </div>
+            </div>
+            <Footer/>
+        </aside>
       </div>
     );
   }
