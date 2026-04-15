ae84dc61edd66d54269a92cf8bece35d792d67fb
jorhdan <dil@DiLs-MacBook-Air.local>
2026-04-15 12:48:06 +0300

    check

--- src/layouts/JobLayout.jsx
+++ src/layouts/JobLayout.jsx
@@ -7,7 +7,7 @@ export default function JobLayout() {
     <div className="flex h-screen overflow-hidden bg-[var(--main-bg)]">
 
       {/* Main */}
-      <div className="flex-1 flex flex-col justify-center">
+      <div className="flex-1 flex flex-col">
 
         {/* Header */}
         <header className="fixed top-0 left-0 w-full z-40 border-b border-[var(--border)]  bg-[var(--header-bg)]">
@@ -24,9 +24,8 @@ export default function JobLayout() {
         </header>
 
         {/* Pages */}
-        <main className="pt-16 bg-[var(--main-bg)] px-4 md:px-6 flex-1 overflow-y-auto">
-          <Outlet />
-        </main>
+        <Outlet />
+        
 
       </div>
     </div>
--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -7,38 +7,41 @@ export default function JobDetailPage() {
 
     const [ blockSkeleton , setBlockSkeleton ] = useState(true);
     return (
-        <div className="w-fulll flex gap-2" >
-        {
-          blockSkeleton 
-          ?  <JobsSkeleton />
-          :  <main
-                className="
-                  w-full
-                  pt-6
-                  lg:w-[710px] 
-                "
-              >
-                <h1>job detail</h1>
-                
-              </main>
-        }
-        <aside className="p-6 hidden lg:flex gap-3 flex-col ">
-            <div className=' mt-4 flex-col  flex gap-2'>
-              <SkeletonBlock className="w-20 h-6 "/>
-              <SkeletonBlock className="w-full h-[150px]"/>
+        <main className="pt-16 bg-[var(--main-bg)] px-4 md:px-6 flex-1 overflow-y-auto">
+            <div className="w-fulll flex  gap-2 justify-center" >
+                {
+                blockSkeleton 
+                ?  <JobsSkeleton />
+                :  <main
+                        className="
+                        w-full
+                        pt-6
+                        lg:w-[710px] 
+                        "
+                    >
+                        <h1>job detail</h1>
+                        
+                    </main>
+                }
+                <aside className="p-6 hidden w-[400px] lg:flex gap-3 flex-col ">
+                    <div className=' mt-4 flex-col  flex gap-2'>
+                    <SkeletonBlock className="w-20 h-6 "/>
+                    <SkeletonBlock className="w-full h-[150px]"/>
+                    </div>
+                    <div className='flex gap-2'>
+                    <div className=''>
+                        <SkeletonBlock className="w-[100px] h-[100px]"/>
+                    </div>
+                    <div className='flex-1 flex flex-col gap-2'>
+                        <SkeletonBlock className="h-5 w-full"/>
+                        <SkeletonBlock className="h-5 w-3/4"/>
+                        <SkeletonBlock className="h-5 w-1/4"/>
+                    </div>
+                    </div>
+                    <Footer/>
+                </aside>
             </div>
-            <div className='flex gap-2'>
-              <div className=''>
-                <SkeletonBlock className="w-[100px] h-[100px]"/>
-              </div>
-              <div className='flex-1 flex flex-col gap-2'>
-                <SkeletonBlock className="h-5 w-full"/>
-                <SkeletonBlock className="h-5 w-3/4"/>
-                <SkeletonBlock className="h-5 w-1/4"/>
-              </div>
-            </div>
-            <Footer/>
-        </aside>
-      </div>
+        </main>
+            
     );
   }
