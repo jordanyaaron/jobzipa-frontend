ca9641cbddaa5c5e1e25e0c7692ee7558a3f705b
jorhdan <dil@DiLs-MacBook-Air.local>
2026-04-16 18:00:39 +0300

    fix

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -74,6 +74,7 @@ export default function JobDetailPage() {
   useEffect(() => {
     if (job) {
       setSaved(isJobSaved(job.public_id));
+      console.log(job.description);
     }
   }, [job]);
 
@@ -220,6 +221,17 @@ export default function JobDetailPage() {
               <div className="mt-4 flex flex-col gap-2">
                 <SkeletonBlock className="w-20 h-6" />
                 <SkeletonBlock className="w-full h-[150px]" />
+              </div>
+
+              <div className='flex mt-3 gap-2'>
+                <div>
+                  <SkeletonBlock className="w-[100px] h-[100px]"/>
+                </div>
+                <div className='flex-1 flex flex-col gap-2'>
+                    <SkeletonBlock className="h-5 w-full"/>
+                    <SkeletonBlock className="h-5 w-3/4"/>
+                    <SkeletonBlock className="h-5 w-1/4"/>
+                </div>
               </div>
 
               <Footer />
