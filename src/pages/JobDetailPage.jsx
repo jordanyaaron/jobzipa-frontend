51e6dd488a0a89201e67457c861a0378f20cac32
jorhdan <dil@192.168.1.7>
2026-04-16 21:36:24 +0300

    fix

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -183,6 +183,7 @@ export default function JobDetailPage() {
                 <div className="p-4">
                 <div
                     className="
+                        ql-editor
                         prose
                         prose-ul:list-disc
                         prose-ol:list-decimal
