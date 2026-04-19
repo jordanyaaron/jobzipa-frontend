03b31f6e484c9b44d59d552a5b7f8a6402182950
jorhdan <dil@192.168.1.7>
2026-04-19 10:02:44 +0300

    remove extra loadin skeleton on sidebar at home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,22 +90,7 @@ const Home = () => {
                     <SkeletonBlock className="h-5 w-1/4"/>
                 </div>
               </div>
-              <div className='mt-4 flex flex-col gap-2'>
-                  <SkeletonBlock className="w-20 h-6"/>
-                  <SkeletonBlock className="w-full h-[150px]"/>
-                  </div>
-
-                  <div className='flex mt-3 gap-2'>
-                  <div>
-                      <SkeletonBlock className="w-[100px] h-[100px]"/>
-                  </div>
-                  <div className='flex-1 flex flex-col gap-2'>
-                      <SkeletonBlock className="h-5 w-full"/>
-                      <SkeletonBlock className="h-5 w-3/4"/>
-                      <SkeletonBlock className="h-5 w-1/4"/>
-                  </div>
-              </div>
-
+            
               <Footer />
 
           </div>
