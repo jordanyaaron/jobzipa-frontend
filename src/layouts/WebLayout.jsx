1f17684fca1cb1464044e3fd3ac49013db2d1151
jorhdan <dil@192.168.1.9>
2026-05-02 10:49:33 +0300

    minimize the padding top of the main body in weblayout

--- src/layouts/WebLayout.jsx
+++ src/layouts/WebLayout.jsx
@@ -37,7 +37,7 @@ export default function InfoLayout() {
       </div>
 
       {/* CONTENT (IMPORTANT SPACING) */}
-      <div className="max-w-5xl mx-auto w-full flex-1 pt-20">
+      <div className="max-w-5xl mx-auto w-full flex-1 pt-2">
         <Outlet />
       </div>
 
