d7feeebdb6ccc4e6798027997bd2979c11f6e09c
jorhdan <dil@192.168.1.20>
2026-05-13 23:49:10 +0300

    add ad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,6 +50,31 @@ const Home = () => {
     fetchJobs();
   }, []);
  
+  useEffect(() => {
+  if (window.location.hostname !== "jobzipa.com") return;
+
+  const script1 = document.createElement("script");
+  script1.innerHTML = `
+    atOptions = {
+      'key' : 'c19acf17a50b54480aeff4aa3a225032',
+      'format' : 'iframe',
+      'height' : 50,
+      'width' : 320,
+      'params' : {}
+    };
+  `;
+
+  
+  const script2 = document.createElement("script");
+  script2.src =
+    "https://www.highperformanceformat.com/c19acf17a50b54480aeff4aa3a225032/invoke.js";
+  script2.async = true;
+
+  if (adRef.current) {
+    adRef.current.appendChild(script1);
+    adRef.current.appendChild(script2);
+  }
+}, []);
  
   return (
     <>
@@ -80,8 +105,9 @@ const Home = () => {
 
                 {/* ADS */}
                 <div className='mt-4 hidden lg:flex flex-col gap-2'>
-                    <SkeletonBlock className="w-20 h-6"/>
-                    <SkeletonBlock className="w-full h-[150px]"/>
+                    <div className="text-xs text-gray-400">Sponsored</div>
+
+                    <div ref={adRef} className="w-[320px] h-[50px] mx-auto" />
                 </div>
 
                 <div className='hidden lg:flex mt-3 gap-2'>
